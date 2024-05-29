dd5f06b33bba02187f7e3eb9f97411b37a2e1633
ericlurker <96100006+ericlurker@users.noreply.github.com>
2024-05-29 16:36:31 +0800

    update, modify core contracts

---
+++ packages/contracts/contracts/interfaces/ICommitmentPool.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./IVerifier.sol";
+
+interface ICommitmentPool {
+  struct CommitmentRequest {
+    uint256 amount;
+    uint256 commitment;
+    uint256 executorFee;
+    uint256 rollupFee;
+    bytes encryptedNote;
+  }
+
+  struct RollupRequest {
+    IVerifier.Proof proof;
+    uint32 rollupSize;
+    uint256 newRoot;
+    uint256 leafHash;
+  }
+
+  struct TransactRequest {
+    IVerifier.Proof proof;
+    uint256 rootHash;
+    uint256[] serialNumbers;
+    uint256[] sigHashes;
+    bytes32 sigPk;
+    uint256 publicAmount;
+    uint256 relayerFeeAmount;
+    uint256[] outCommitments;
+    uint256[] outRollupFees;
+    address publicRecipient;
+    address relayerAddress;
+    bytes[] outEncryptedNotes;
+    uint256 randomAuditingPublicKey;
+    uint256[] encryptedAuditorNotes;
+  }
+
+  function enqueue(CommitmentRequest memory _request, address _executor) external;
+
+  function rollup(RollupRequest memory _request) external;
+
+  function transact(TransactRequest memory _request, bytes memory _signature) external;
+}
---
+++ packages/contracts/contracts/interfaces/IMystikoLoop.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IMystikoLoop {
+  struct LocalConfig {
+    uint256 minAmount;
+    uint256 maxAmount;
+  }
+
+  struct DepositRequest {
+    uint256 amount;
+    uint256 commitment;
+    uint256 hashK;
+    uint128 randomS;
+    bytes encryptedNote;
+    uint256 rollupFee;
+  }
+
+  function deposit(DepositRequest memory _request) external payable;
+
+  function depositWithCertificate(
+    DepositRequest memory _request,
+    uint256 certificateDeadline,
+    bytes memory certificateSignature
+  ) external payable;
+}
