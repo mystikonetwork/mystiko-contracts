22b7a20eee7b045527f118c1cf452fd9bd1f4bcc
ericlurker <96100006+ericlurker@users.noreply.github.com>
2024-07-17 15:12:34 +0800

    update, change contract lib dependency

--- packages/contracts-roller/contracts/MystikoRollerPool.sol
+++ packages/contracts-roller/contracts/MystikoRollerPool.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.26;
 
 import {IMystikoRollerPool, RollerValidateParams} from "./interfaces/IMystikoRollerPool.sol";
 import {MystikoRollerPoolErrors} from "./MystikoRollerPoolErrors.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {MystikoDAOAccessControl} from "@mystikonetwork/contracts-governance/contracts/MystikoDAOAccessControl.sol";
 
 contract MystikoRollerPool is IMystikoRollerPool, MystikoDAOAccessControl {
