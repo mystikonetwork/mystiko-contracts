03712c9d589e44d64be12b2df1078f91ba8eb459
ericlurker <96100006+ericlurker@users.noreply.github.com>
2024-10-11 14:21:30 +0800

    update, add mock test token contracts

---
+++ packages/contracts/contracts/mocks/token/MockTestToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MockTestToken is ERC20, ERC20Permit {
+  uint8 private _decimal = 18;
+
+  constructor(string memory symbol, uint8 decimal) ERC20("Mystiko Test Token", symbol) ERC20Permit("Mystiko Test Token") {
+    _decimal = decimal;
+    _mint(msg.sender, 1000 * 1000 * 1000 * (10 ** _decimal));
+  }
+
+  function decimals() public view override returns (uint8) {
+    return _decimal;
+  }
+}
